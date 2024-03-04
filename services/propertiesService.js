72163d616b231029e276fa322d9ae8b6e6bdd2d2
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-04 12:07:25 +0100

    Properties service proofing when api is null

--- services/propertiesService.js
+++ services/propertiesService.js
@@ -1,27 +1,34 @@
+const api = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
 export async function fetchProperties() {
 	try {
-		const res = await fetch(
-			`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
-		);
+		if (!api) {
+			return [];
+		}
+
+		const res = await fetch(`${api}/properties`);
 		if (!res.ok) {
 			throw new Error("Failed to fetch data");
 		}
 		return res.json();
 	} catch (error) {
 		console.log("SERVICE:(/properties) Error: ", error);
+		return [];
 	}
 }
 
 export async function fetchProperty(id) {
 	try {
-		const res = await fetch(
-			`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties/${id}`
-		);
+		if (!api) {
+			return {};
+		}
+		const res = await fetch(`${api}/properties/${id}`);
 		if (!res.ok) {
 			throw new Error("Failed to fetch data");
 		}
 		return res.json();
 	} catch (error) {
 		console.log("SERVICE:(/properties/:id) Error: ", error);
+		return {};
 	}
 }
