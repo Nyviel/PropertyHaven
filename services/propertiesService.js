a8d05f2640325cd6831e733783c9ecdded8f4e57
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-13 13:36:42 +0100

    Added methods to hit delete/put endpoints for props

--- services/propertiesService.js
+++ services/propertiesService.js
@@ -48,3 +48,40 @@ export async function fetchPropertiesByUID(uid) {
 		return [];
 	}
 }
+
+export async function deleteProperty(id) {
+	try {
+		if (!api) {
+			return false;
+		}
+		const res = await fetch(`${api}/properties/${id}`, {
+			method: "DELETE",
+		});
+		if (!res.ok) {
+			throw new Error("Failed to delete property");
+		}
+		return true;
+	} catch (error) {
+		console.log("SERVICE:(/properties/:id Error: ", error);
+		return false;
+	}
+}
+
+export async function updateProperty(id, formData) {
+	try {
+		if (!api) {
+			return null;
+		}
+		const res = await fetch(`${api}/properties/${id}`, {
+			method: "PUT",
+			body: formData,
+		});
+		if (!res.ok) {
+			throw new Error("Failed to update property");
+		}
+		return res.json();
+	} catch (error) {
+		console.log("SERVICE:(/properties/:id Error: ", error);
+		return null;
+	}
+}
