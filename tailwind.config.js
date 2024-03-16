f59ba1b77db27577ef3e19ca456ad490b521b801
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-16 19:25:09 +0100

    Next UI config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
 module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: ["Poppins", "sans-serif"],
-			},
 			gridTemplateColumns: {
 				"70/30": "70% 28%",
 			},
 		},
 	},
-	plugins: [],
+	darkMode: "class",
+	plugins: [nextui()],
 };
