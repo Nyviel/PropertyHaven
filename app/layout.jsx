165f54795c8eca3bd470c1715432adee5d40105f
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-16 19:25:25 +0100

    Added Inter and Poppins fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,9 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { inter } from "./ui/inter";
+import { poppins } from "./ui/poppins";
+import NextUIWrapper from "@/components/NextUIWrapper";
 
 export const metadata = {
 	title: "Property Haven | Your go to rental website",
@@ -16,10 +19,14 @@ const MainLayout = ({ children }) => {
 	return (
 		<AuthProvider>
 			<html lang="en">
-				<body className="flex flex-col bg-blue-50">
-					<Navbar />
-					<main className="flex-1">{children}</main>
-					<Footer />
+				<body
+					className={`${poppins.className} ${inter.className} antialiased flex flex-col bg-blue-50`}
+				>
+					<NextUIWrapper>
+						<Navbar />
+						<main className="flex-1">{children}</main>
+						<Footer />
+					</NextUIWrapper>
 					<ToastContainer
 						position="bottom-right"
 						autoClose={5000}
