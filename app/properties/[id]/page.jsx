fa9fcff595468050336aa5aee5a7cc7580503dfe
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-23 20:23:26 +0100

    Adjusted padding size on property details

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -60,7 +60,7 @@ const PropertyPage = () => {
 						</div>
 					</section>
 					<section className="bg-blue-50">
-						<div className="container m-auto py-10 px-6">
+						<div className="container m-auto py-4 px-6">
 							<div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
 								<PropertyDetails property={property} />
 								{/* <!-- Sidebar --> */}
