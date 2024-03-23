01c7113f83ca35f01c9dc7c88dc7f23c0d57e0dc
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-23 20:22:15 +0100

    Bookmark component now checks bookmark status

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,6 +1,6 @@
-import { toggleBookmark } from "@/services/bookmarkService";
+import { checkBookmark, toggleBookmark } from "@/services/bookmarkService";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBookmark } from "react-icons/fa";
 import { toast } from "react-toastify";
 
@@ -8,6 +8,20 @@ const BookmarkButton = ({ property }) => {
 	const { data: session } = useSession();
 	const uid = session?.user?.id;
 	const [isBookmarked, setIsBookmarked] = useState(false);
+
+	useEffect(() => {
+		const bookmarkCheck = async () => {
+			if (session && session.user) {
+				const res = await checkBookmark(property);
+				console.log(res);
+				if (res !== null) {
+					setIsBookmarked(res.isBookmarked);
+				}
+			}
+		};
+		bookmarkCheck();
+	}, [session]);
+
 	const handleClick = async () => {
 		if (!uid) {
 			toast.error("You need to sign in to bookmark a property");
@@ -31,7 +45,11 @@ const BookmarkButton = ({ property }) => {
 			onClick={() => {
 				handleClick();
 			}}
-			className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+			className={`${
+				!isBookmarked
+					? "bg-blue-500 hover:bg-blue-600"
+					: "bg-red-500 hover:bg-red-600"
+			}  text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center`}
 		>
 			<FaBookmark className="inline-block mr-2" />{" "}
 			{isBookmarked ? "Remove Bookmark" : "Bookmark Property"}
