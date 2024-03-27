da81f6abc02b6964278be3c5362b823d874ac60b
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-03-27 14:58:35 +0100

    Visual overhaul of user profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -11,6 +11,8 @@ import {
 } from "@/services/propertiesService";
 import Spinner from "@/components/Spinner";
 import { toast } from "react-toastify";
+import ProfileListing from "@/components/ProfileListing";
+import { FaArrowLeft } from "react-icons/fa";
 
 const ProfilePage = () => {
 	const { data: session } = useSession();
@@ -57,41 +59,49 @@ const ProfilePage = () => {
 			alert("Failed to delete property");
 		}
 	};
+
 	return (
-		<section className="">
-			<div className="container m-auto py-12">
-				<div className="bg-primary-100 px-6 py-8 mb-4 shadow-lg shadow-primary-300 rounded-md m-4 md:m-0">
-					<h1 className="text-3xl font-bold mx-10 mb-10 lg:mb-20 text-primary-700 text-center">
+		<section>
+			<div className="bg-primary-500">
+				<div className="container mx-auto min-h-48 text-center py-5">
+					<Link
+						href="/"
+						className="flex items-center text-white hover:underline py-3"
+					>
+						<FaArrowLeft className="mr-2" /> Return to Home
+					</Link>
+					<h1 className="text-3xl font-bold mx-10 my-10 text-primary-950 text-center">
 						Your Profile
 					</h1>
+					<div className="w-full flex flex-col items-center justify-center">
+						<Image
+							src={ProfileDefault}
+							alt="User"
+							height={0}
+							width={0}
+							sizes={"100vw"}
+							className="w-48 h-48 rounded-full"
+						></Image>
+						<h2 className="text-xl mt-6 mb-4 text-white">
+							<span className="font-bold block text-primary-950">
+								Name
+							</span>{" "}
+							{session ? session.user?.name : ""}
+						</h2>
+						<h2 className="text-xl mb-4 text-white">
+							<span className="font-bold block text-primary-950">
+								Email
+							</span>{" "}
+							{session ? session.user?.email : ""}
+						</h2>
+					</div>
+				</div>
+			</div>
+			<div className="container m-auto my-12">
+				<div className="px-6 py-8 mb-4 m-4 md:m-0">
 					<div className="flex flex-col md:flex-row">
-						<div className="md:w-1/4 md:mx-10 mx-auto text-center md:text-left mb-10">
-							<div className="mb-4">
-								<Image
-									className="h-32 w-32 lg:h-48 lg:w-48 md:mx-0 rounded-full "
-									src={ProfileDefault}
-									alt="User"
-									height={0}
-									width={0}
-									sizes={"100vw"}
-								/>
-							</div>
-							<h2 className="text-xl mb-4 text-primary-800">
-								<span className="font-bold block text-primary-700">
-									Name
-								</span>{" "}
-								{session ? session.user?.name : ""}
-							</h2>
-							<h2 className="text-xl text-primary-800">
-								<span className="font-bold block text-primary-700">
-									Email
-								</span>{" "}
-								{session ? session.user?.email : ""}
-							</h2>
-						</div>
-
-						<div className="md:w-3/4 md:pl-4">
-							<h2 className="text-xl font-semibold mb-4 text-primary-700">
+						<div className="w-full md:pl-4">
+							<h2 className="text-2xl font-semibold mb-4 text-primary-700">
 								Your Listings
 							</h2>
 							{!properties.length && loading && (
@@ -112,61 +122,19 @@ const ProfilePage = () => {
 									</Link>
 								</div>
 							) : (
-								properties.map((property, index) => {
-									return (
-										<div
-											key={index}
-											className="mb-10 bg-primary-50 shadow-lg shadow-primary-300 p-4 rounded-md"
-										>
-											<Link
-												href={`/properties/${property._id}`}
-											>
-												<Image
-													className="h-32 w-full rounded-md object-cover"
-													src={property.images[0]}
-													alt="Property Thumbnail"
-													height={0}
-													width={0}
-													sizes={"100vw"}
-												/>
-											</Link>
-											<div className="mt-2">
-												<p className="text-lg text-primary-800 font-semibold">
-													{property.name}
-												</p>
-												<p className="text-primary-600">
-													Address:{" "}
-													{`${property.location.street} ${property.location.city}, ${property.location.state}`}
-												</p>
-											</div>
-											<div className="mt-2">
-												<Link
-													href={`/properties/${property._id}/edit`}
-													className="bg-orange-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-orange-600"
-												>
-													Edit
-												</Link>
-												<Link
-													href={`/properties/${property._id}`}
-													className="bg-primary-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-primary-600"
-												>
-													Details
-												</Link>
-												<button
-													className="bg-red-500 text-white px-3 py-3 rounded-md hover:bg-red-600"
-													type="button"
-													onClick={() => {
-														handleDeleteProperty(
-															property._id
-														);
-													}}
-												>
-													Delete
-												</button>
-											</div>
-										</div>
-									);
-								})
+								<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+									{properties.map((property, index) => {
+										return (
+											<ProfileListing
+												property={property}
+												deleteHandler={
+													handleDeleteProperty
+												}
+												key={index}
+											/>
+										);
+									})}
+								</div>
 							)}
 						</div>
 					</div>
