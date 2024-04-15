2ac391f33f836532ee1e9863c48dce67c13539b0
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-04-15 11:51:39 +0200

    Temporarily restricted website

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,16 @@
 import "@/assets/styles/globals.css";
-import AuthProvider from "@/components/AuthProvider";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import { Bounce, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "photoswipe/dist/photoswipe.css";
-import { inter } from "./ui/inter";
-import { poppins } from "./ui/poppins";
-import NextUIWrapper from "@/components/NextUIWrapper";
-import NextThemesWrapper from "@/components/NextThemesWrapper";
-import { GlobalProvider } from "@/context/GlobalContext";
+// import AuthProvider from "@/components/AuthProvider";
+// import Footer from "@/components/Footer";
+// import Navbar from "@/components/Navbar";
+// import { Bounce, ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import "photoswipe/dist/photoswipe.css";
+// import { inter } from "./ui/inter";
+// import { poppins } from "./ui/poppins";
+// import NextUIWrapper from "@/components/NextUIWrapper";
+// import NextThemesWrapper from "@/components/NextThemesWrapper";
+// import { GlobalProvider } from "@/context/GlobalContext";
+import UnderConstruction from "@/components/UnderConstruction";
 export const metadata = {
 	title: "Property Haven | Your go to rental website",
 	description: "Browse, rent, buy and sell properties on Property Haven!",
@@ -19,36 +20,37 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
 	return (
-		<GlobalProvider>
-			<AuthProvider>
-				<html lang="en" suppressHydrationWarning={true}>
-					<body
-						className={`${poppins.className} ${inter.className} antialiased height-screen bg-background`}
-					>
-						<NextUIWrapper>
-							<NextThemesWrapper>
-								<Navbar />
-								<main className="flex-1">{children}</main>
-								<Footer />
-							</NextThemesWrapper>
-						</NextUIWrapper>
-						<ToastContainer
-							position="bottom-right"
-							autoClose={5000}
-							hideProgressBar={false}
-							newestOnTop={false}
-							closeOnClick
-							rtl={false}
-							pauseOnFocusLoss
-							draggable
-							pauseOnHover
-							theme="colored"
-							transition={Bounce}
-						/>
-					</body>
-				</html>
-			</AuthProvider>
-		</GlobalProvider>
+		<UnderConstruction />
+		// <GlobalProvider>
+		// 	<AuthProvider>
+		// 		<html lang="en" suppressHydrationWarning={true}>
+		// 			<body
+		// 				className={`${poppins.className} ${inter.className} antialiased height-screen bg-background`}
+		// 			>
+		// 				<NextUIWrapper>
+		// 					<NextThemesWrapper>
+		// 						<Navbar />
+		// 						<main className="flex-1">{children}</main>
+		// 						<Footer />
+		// 					</NextThemesWrapper>
+		// 				</NextUIWrapper>
+		// 				<ToastContainer
+		// 					position="bottom-right"
+		// 					autoClose={5000}
+		// 					hideProgressBar={false}
+		// 					newestOnTop={false}
+		// 					closeOnClick
+		// 					rtl={false}
+		// 					pauseOnFocusLoss
+		// 					draggable
+		// 					pauseOnHover
+		// 					theme="colored"
+		// 					transition={Bounce}
+		// 				/>
+		// 			</body>
+		// 		</html>
+		// 	</AuthProvider>
+		// </GlobalProvider>
 	);
 };
 
---
+++ components/UnderConstruction.jsx
@@ -0,0 +1,4 @@
+const UnderConstruction = () => {
+	return <div>This website is currently under construction.</div>;
+};
+export default UnderConstruction;
