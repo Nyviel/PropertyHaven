e7af1def22095b546a44b2cffd03913d50bb3167
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-09-12 14:29:30 +0200

    Restore app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,15 @@
 import "@/assets/styles/globals.css";
-// import AuthProvider from "@/components/AuthProvider";
-// import Footer from "@/components/Footer";
-// import Navbar from "@/components/Navbar";
-// import { Bounce, ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import "photoswipe/dist/photoswipe.css";
-// import { inter } from "./ui/inter";
-// import { poppins } from "./ui/poppins";
-// import NextUIWrapper from "@/components/NextUIWrapper";
-// import NextThemesWrapper from "@/components/NextThemesWrapper";
-// import { GlobalProvider } from "@/context/GlobalContext";
-import UnderConstruction from "@/components/UnderConstruction";
+import AuthProvider from "@/components/AuthProvider";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
+import { inter } from "./ui/inter";
+import { poppins } from "./ui/poppins";
+import NextUIWrapper from "@/components/NextUIWrapper";
+import NextThemesWrapper from "@/components/NextThemesWrapper";
+import { GlobalProvider } from "@/context/GlobalContext";
 export const metadata = {
 	title: "Property Haven | Your go to rental website",
 	description: "Browse, rent, buy and sell properties on Property Haven!",
@@ -20,37 +19,36 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
 	return (
-		<UnderConstruction />
-		// <GlobalProvider>
-		// 	<AuthProvider>
-		// 		<html lang="en" suppressHydrationWarning={true}>
-		// 			<body
-		// 				className={`${poppins.className} ${inter.className} antialiased height-screen bg-background`}
-		// 			>
-		// 				<NextUIWrapper>
-		// 					<NextThemesWrapper>
-		// 						<Navbar />
-		// 						<main className="flex-1">{children}</main>
-		// 						<Footer />
-		// 					</NextThemesWrapper>
-		// 				</NextUIWrapper>
-		// 				<ToastContainer
-		// 					position="bottom-right"
-		// 					autoClose={5000}
-		// 					hideProgressBar={false}
-		// 					newestOnTop={false}
-		// 					closeOnClick
-		// 					rtl={false}
-		// 					pauseOnFocusLoss
-		// 					draggable
-		// 					pauseOnHover
-		// 					theme="colored"
-		// 					transition={Bounce}
-		// 				/>
-		// 			</body>
-		// 		</html>
-		// 	</AuthProvider>
-		// </GlobalProvider>
+		<GlobalProvider>
+			<AuthProvider>
+				<html lang="en" suppressHydrationWarning={true}>
+					<body
+						className={`${poppins.className} ${inter.className} antialiased bg-background`}
+					>
+						<NextUIWrapper>
+							<NextThemesWrapper>
+								<Navbar />
+								<main className="flex-1">{children}</main>
+								<Footer />
+							</NextThemesWrapper>
+						</NextUIWrapper>
+						<ToastContainer
+							position="bottom-right"
+							autoClose={5000}
+							hideProgressBar={false}
+							newestOnTop={false}
+							closeOnClick
+							rtl={false}
+							pauseOnFocusLoss
+							draggable
+							pauseOnHover
+							theme="colored"
+							transition={Bounce}
+						/>
+					</body>
+				</html>
+			</AuthProvider>
+		</GlobalProvider>
 	);
 };
 
